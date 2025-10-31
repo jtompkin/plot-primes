970fc299f825c987e1d7bc0b41331653f353bbe9
jtompkin <jtompkin-dev@pm.me>
2025-10-30 20:24:28 -0400

    update nix script

--- nix/plotprimes.py
+++ nix/plotprimes.py
@@ -5,34 +5,44 @@
 # This is a reproducible interpreted script that only requires Nix to run.
 # See: https://nix.dev/tutorials/first-steps/reproducible-scripts.html
 import argparse
+from math import log
 from typing import Generator
 
 from matplotlib import pyplot as plt
 import numpy as np
 
 
-def is_prime(x: int) -> bool:
-    if x < 2:
-        return False
-    for divisor in range(2, int(x**0.5) + 1):
-        if x % divisor == 0:
-            return False
-    return True
+def get_upper_limit(n: int) -> int:
+    """Get the approximate upper limit of the `n`th prime number"""
+    if n < 6:
+        return 15
+    return int(n * (log(n) + log(log(n)))) + 1
 
 
 def get_primes(n: int) -> Generator[int]:
-    count = 0
-    i = 2
-    while count < n:
-        if is_prime(i):
-            count += 1
-            yield i
-        i += 1
+    """
+    Get at least `n` prime numbers. Will return more than `n` numbers due to
+    approximating the upper limit of the `n`th prime number. You can truncate the list
+    by doing something like this:
+    ```python
+    n = 10000
+    list(get_primes(n))[:n]
+    ```
+    """
+    limit = get_upper_limit(n)
+    is_prime = np.ones(limit, dtype=np.bool)
+    is_prime[:2] = np.bool(False)
+    for i in range(2, int(limit**0.5) + 1):
+        if is_prime[i]:
+            is_prime[i * i :: i] = np.bool(False)
+    return (i for i, b in enumerate(is_prime) if b)
 
 
 def plot_primes(n: int, colormap: str) -> None:
     fig, ax = plt.subplots(subplot_kw={"projection": "polar"})
-    ax.scatter(list(get_primes(n)), np.arange(n), s=1, c=np.arange(n), cmap=colormap)
+    ax.scatter(
+        list(get_primes(n))[:n], np.arange(n), s=0.5, c=np.arange(n), cmap=colormap
+    )
     ax.set_axis_off()
 
     fig.set_facecolor("black")
